3b5ed32d8ec68f22355e4d2e41c0b9c26f388deb
Dave Gumba <dave_gumba@Yahoo.com>
2019-08-30 18:03:06 -0400

    first commit

---
+++ Sententia/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Sententia
+//
+//  Created by Dave Gumba on 2019-08-30.
+//  Copyright © 2019 Dave's Organization. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
